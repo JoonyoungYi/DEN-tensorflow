9d1f202224def75508907c9f5738d234aa09da44
Joonyoung Yi <joonyoung.yi@kaist.ac.kr>
2018-06-19 02:08:13 -0400

    gitignore

--- run.py
+++ run.py
@@ -33,6 +33,7 @@ def main():
         "The number of units increased in the expansion processing")
     flags.DEFINE_float('loss_thr', 0.01, "Threshold of dynamic expansion")
     flags.DEFINE_float('spl_thr', 0.05, "Threshold of split and duplication")
+    flags.DEFINE_float('n_task', 100, "Threshold of split and duplication")
     FLAGS = flags.FLAGS
 
     mnist = input_data.read_data_sets('data', one_hot=True)
@@ -41,11 +42,11 @@ def main():
     testX = mnist.test.images
 
     task_permutation = []
-    for task in range(10):
+    for task in range(FLAGS.n_task):
         task_permutation.append(np.random.permutation(784))
 
     trainXs, valXs, testXs = [], [], []
-    for task in range(10):
+    for task in range(FLAGS.n_task):
         trainXs.append(trainX[:, task_permutation[task]])
         valXs.append(valX[:, task_permutation[task]])
         testXs.append(testX[:, task_permutation[task]])
@@ -54,7 +55,7 @@ def main():
     params = dict()
     avg_perf = []
 
-    for t in range(FLAGS.n_classes):
+    for t in range(FLAGS.n_task):
         data = (trainXs[t], mnist.train.labels, valXs[t],
                 mnist.validation.labels, testXs[t], mnist.test.labels)
         model.sess = tf.Session()
@@ -85,4 +86,6 @@ def main():
 
 
 if __name__ == '__main__':
+    sys.stdout = open('output.txt', 'a')
     main()
+    sys.stdout.close()
